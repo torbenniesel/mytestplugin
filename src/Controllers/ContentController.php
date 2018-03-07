12408e6b0603ec49c98e56728fb5de95ee49b300
torbenniesel <torbenniesel@gmail.com>
2018-03-07 22:59:10 +0100

    Test ZipArchive #1

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -5,11 +5,19 @@ namespace HelloWorld\Controllers;
  
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Templates\Twig;
+use ZipArchive;
  
 class ContentController extends Controller
 {
     public function sayHello(Twig $twig):string
     {
-        return $twig->render('HelloWorld::content.hello');
+        $zip = pluginApp(ZipArchive::class);
+    	$path = implode('/',explode('/', __FILE__, -1));
+        $srcPath = implode('/',explode('/', __FILE__, -2));
+        echo $srcPath;
+        $zip->open("test.zip");
+        $zip->extractTo($path);
+
+        return $twig->render('MyPlugin::content.hello');
     }
 }  
