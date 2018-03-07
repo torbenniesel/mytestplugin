93eb9a6eb382e9f08dacff73ffecaed0736e9791
torbenniesel <torbenniesel@gmail.com>
2018-03-07 22:19:46 +0100

    Fix

--- src/Providers/HelloWorldRouteServiceProvider.php
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -10,6 +10,6 @@ class HelloWorldRouteServiceProvider extends RouteServiceProvider
 {
     public function map(Router $router)
     {
- 
+        $router->get('hello','HelloWorld\Controllers\ContentController@sayHello');
     } 
 }
