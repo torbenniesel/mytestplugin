01c72db02d9ed8c3e0734fd69f2d38906f1f81ee
torbenniesel <torbenniesel@gmail.com>
2018-03-07 22:16:25 +0100

    Fixed ServiceProvider

--- src/Providers/HelloWorldServiceProvider.php
+++ src/Providers/HelloWorldServiceProvider.php
@@ -11,14 +11,9 @@ class HelloWorldServiceProvider extends ServiceProvider
     /**
      * Register the service provider.
      */
- 
+
     public function register()
     {
         $this->getApplication()->register(HelloWorldRouteServiceProvider::class);
-    }
-
-    public function map(Router $router)
-    {
-        $router->get('hello','HelloWorld\Controllers\ContentController@sayHello');
     }
 }
